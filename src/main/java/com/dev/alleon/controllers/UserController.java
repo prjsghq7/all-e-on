217b4a6d33da51185fda7c0b933255c96316dc97
ImDongHo <imtongho1@gmail.com>
2025-08-03 03:20:55 +0900

    [임동호, 검색 박스 구현] 검색 박스 구현, 어노테이션 코드 추가

--- src/main/java/com/dev/alleon/controllers/UserController.java
+++ src/main/java/com/dev/alleon/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.dev.alleon.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@RequestMapping(value = "/user")
 public class UserController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public String getLogin() {
+        return "user/login";
+    }
 }
--- src/main/java/com/dev/alleon/mappers/UserMapper.java
+++ src/main/java/com/dev/alleon/mappers/UserMapper.java
@@ -1,4 +1,7 @@
 package com.dev.alleon.mappers;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
 }
