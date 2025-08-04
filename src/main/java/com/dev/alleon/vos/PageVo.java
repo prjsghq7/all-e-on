8f03b1305d9f57a163433f96b7dacc60ed963e0b
prjsghq7 <prjsghq7@gmail.com>
2025-08-04 19:57:41 +0900

    [김건호, 복지 목록 검색] 페이지 표출 시, 최대 10개까지 표출되도록 수정 현제 선택된 페이지 중심으로 좌우로 페이지가 표출 되도록 수정

--- src/main/java/com/dev/alleon/vos/PageVo.java
+++ src/main/java/com/dev/alleon/vos/PageVo.java
@@ -7,11 +7,32 @@ public class PageVo {
     public final int maxPage;       // 이동 가능한 최대 페이지 번호
     public final int dbOffset;      // SELECT 시 OFFSET 값
 
+    public final int pageSize = 10; // 한 번에 보여줄 페이지 번호 수
+    public int startPage;     // 페이지 표출 시, 첫 페이지 번호
+    public int endPage;       // 페이지 표출 시, 마지막 페이지 번호
+
     public PageVo(int rowCount, int page, int totalCount) {
         this.rowCount = rowCount;
         this.totalCount = totalCount;
         this.maxPage = totalCount / rowCount + (totalCount % rowCount == 0 ? 0 : 1);
         this.page = Math.min(page, this.maxPage);
         this.dbOffset = Math.max(0, (this.page - 1) * rowCount);
+
+        int halfBlock = pageSize / 2;
+
+        this.startPage = page - halfBlock;
+        this.endPage = page + halfBlock - 1;
+
+        if (this.startPage < 1) {
+            this.endPage += 1 - this.startPage;
+            this.startPage = 1;
+        }
+
+        if (this.endPage > maxPage) {
+            this.startPage -= this.endPage - maxPage;
+            this.endPage = maxPage;
+        }
+
+        this.startPage = Math.max(1, this.startPage);
     }
 }
