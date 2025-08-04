6783299c03b9881974b3d88734aafc0672a27517
ImDongHo <imtongho1@gmail.com>
2025-08-04 15:26:48 +0900

    [임동호, 로딩 파일 추가] 로딩 파일 추가

---
+++ src/main/resources/static/common/assets/scripts/common.js
@@ -0,0 +1,78 @@
+/*--------------------------------------------------------------------------------------------
+*     <script defer src="/common/assets/scripts/common.js" type="module"></script> 로 사용
+* --------------------------------------------------------------------------------------------*/
+
+HTMLElement.INVALID_ATTR_NAME = 'data-rb-invalid';
+HTMLElement.VALID_ATTR_NAME = 'data-rb-valid';
+HTMLElement.VISIBLE_ATTR_NAME = 'data-rb-visible';
+
+/**
+ * @param {boolean} b
+ * @returns {HTMLElement} */
+HTMLElement.prototype.setDisabled = function (b) {
+    if (b === true) {
+        this.setAttribute('disabled', '');
+    } else if (b === false) {
+        this.removeAttribute('disabled');
+    }
+    return this;
+}
+
+/**@returns {boolean} */
+HTMLElement.prototype.isInValid = function () {
+    return this.hasAttribute(HTMLElement.INVALID_ATTR_NAME);
+}
+/**
+ * @param {boolean} b
+ * @param {string|undefined} warningText
+ * @returns {HTMLElement} */
+HTMLElement.prototype.setInValid = function (b, warningText = undefined) {
+    this.removeAttribute(HTMLElement.VALID_ATTR_NAME);
+    if (b === true) {
+        const $warning = this.querySelector(':scope > .--warning');
+        if ($warning != null && warningText != null) {
+            $warning.innerText = warningText;
+        }
+        this.setAttribute(HTMLElement.INVALID_ATTR_NAME, '');
+    } else if (b === false) {
+        this.removeAttribute(HTMLElement.INVALID_ATTR_NAME);
+    }
+    return this;
+}
+
+
+/**@returns {boolean} */
+HTMLElement.prototype.isValid = function () {
+    return this.hasAttribute(HTMLElement.VALID_ATTR_NAME);
+}
+/**
+ * @param {boolean} b
+ * @returns {HTMLElement} */
+HTMLElement.prototype.setValid = function (b) {
+    this.removeAttribute(HTMLElement.INVALID_ATTR_NAME);
+    if (b === true) {
+        this.setAttribute(HTMLElement.VALID_ATTR_NAME, '');
+    } else if (b === false) {
+        this.removeAttribute(HTMLElement.VALID_ATTR_NAME);
+    }
+    return this;
+}
+
+/**@returns {boolean} */
+HTMLElement.prototype.isVisible = function () {
+    return this.hasAttribute(HTMLElement.VISIBLE_ATTR_NAME);
+}
+/**
+ * @param {boolean} b
+ * @returns {HTMLElement} */
+HTMLElement.prototype.setVisible = function (b) {
+    if (b === true) {
+        this.setAttribute(HTMLElement.VISIBLE_ATTR_NAME, '');
+    } else if (b === false) {
+        this.removeAttribute(HTMLElement.VISIBLE_ATTR_NAME);
+    }
+    return this;
+}
+
+import { Loading } from './loading.js';
+window.loading = new Loading();
---
+++ src/main/resources/static/common/assets/scripts/loading.js
@@ -0,0 +1,46 @@
+export class Loading {
+    #$element;
+    #$caption;
+
+    constructor() {
+        this.#$element = document.createElement('div');
+        this.#$element.dataset.mtObject = "loading";
+        this.#$element.setVisible(false); // 초기 숨김
+
+        const $icon = document.createElement('img');
+        $icon.dataset.mtComponent = "loading.icon";
+        // 테스트용 임시 이미지
+        $icon.src = '/common/assets/images/button-images/check.png';
+
+        this.#$caption = document.createElement('span');
+        this.#$caption.dataset.mtComponent = "loading.caption";
+        this.#$caption.textContent = '잠시만 기다려주세요.';
+
+        this.#$element.append($icon, this.#$caption);
+        document.body.appendChild(this.#$element);
+    }
+
+    /**
+     * 로딩 표시
+     * @param {string} message
+     */
+    show(message = '잠시만 기다려주세요.') {
+        this.#$caption.textContent = message;
+        this.#$element.setVisible(true);
+    }
+
+    /**
+     * 로딩 숨김
+     */
+    hide() {
+        this.#$element.setVisible(false);
+    }
+
+    /**
+     * 로딩 표시 여부
+     * @returns {boolean}
+     */
+    isVisible() {
+        return this.#$element.isVisible();
+    }
+}
