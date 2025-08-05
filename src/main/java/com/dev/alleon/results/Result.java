f4068ca8230e52fe5a3fea5e63f716d9ba39b228
kwRyu.dev <rkw2115@gmail.com>
2025-08-05 23:07:29 +0900

    [류경완, Result Interface 추가]  - Result Interface 추가

--- src/main/java/com/dev/alleon/results/Result.java
+++ src/main/java/com/dev/alleon/results/Result.java
@@ -1,4 +1,11 @@
 package com.dev.alleon.results;
 
 public interface Result {
+    String name();
+
+    String toString();
+
+    default String toStringLower() {
+        return this.toString().toLowerCase();
+    }
 }
