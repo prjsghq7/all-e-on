e2b0be1fdf45e725d975f4a92952ddabac5794fd
kwRyu.dev <rkw2115@gmail.com>
2025-08-06 18:17:59 +0900

    [류경완, TestConfig 불필요 코드 제거]  - TestConfig 불필요 코드 제거

--- src/main/java/com/dev/alleon/config/TestConfig.java
+++ src/main/java/com/dev/alleon/config/TestConfig.java
@@ -1,14 +1,12 @@
 package com.dev.alleon.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class TestConfig {
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
-    }
+//    @Bean
+//    public WebClient webClient() {
+//        return WebClient.builder().build();
+//    }
 
 }
