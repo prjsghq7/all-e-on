48d33c45d033e9d8253e2e90b1f2589551bf4841
정현규 <hyeongyu-98@naver.com>
2025-08-13 15:14:12 +0900

    [정현규, controller] - controller에 index 추가했습니다.

--- src/main/java/com/dev/alleon/controllers/articleController/articleController.java
+++ src/main/java/com/dev/alleon/controllers/articleController/articleController.java
@@ -1,9 +1,15 @@
 package com.dev.alleon.controllers.articleController;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/article")
 public class articleController {
+    @RequestMapping(value = "/",method= RequestMethod.GET,produces = MediaType.TEXT_HTML_VALUE)
+    public String getIndex(){
+        return "article/article";
+    }
 }
--- src/main/java/com/dev/alleon/controllers/noticeController/noticeController.java
+++ src/main/java/com/dev/alleon/controllers/noticeController/noticeController.java
@@ -1,9 +1,15 @@
 package com.dev.alleon.controllers.noticeController;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/notice")
 public class noticeController {
+    @RequestMapping(value = "/",method = RequestMethod.GET,produces = MediaType.TEXT_HTML_VALUE)
+    public String getIndex(){
+        return "notice/notice";
+    }
 }
