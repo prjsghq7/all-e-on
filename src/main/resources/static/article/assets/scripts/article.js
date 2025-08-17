4e014ff7d5bf8ff3b06b9b92f8ee5baa5378c554
prjsghq7 <prjsghq7@gmail.com>
2025-08-18 01:41:42 +0900

    [김건호, 게시글 댓글 등록] 댓글 및 대댓글 디자인 및 목록 가져오기 추가

--- src/main/resources/static/article/assets/scripts/article.js
+++ src/main/resources/static/article/assets/scripts/article.js
@@ -1,6 +1,8 @@
-const $commentForm = document.getElementById("commentForm");
 const token = document.querySelector('meta[name="_csrf"]').getAttribute('content');
 const header = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+
+const $commentForm = document.getElementById("commentForm");
+const $commentContainer = document.getElementById('commentContainer');
 
 $commentForm.addEventListener('submit' , (e) => {
    e.preventDefault();
@@ -26,9 +28,39 @@ $commentForm.addEventListener('submit' , (e) => {
         }
 
         const response = JSON.parse(xhr.responseText);
-        alert(response);
+        alert(response.result);
     };
     xhr.open('POST', '/api/article/comment/upload');
     xhr.setRequestHeader(header, token);
     xhr.send(formData);
+});
+
+const loadComments = (page) => {
+    const xhr = new XMLHttpRequest();
+
+    const articleIndex = new URL(location.href).searchParams.get('index')
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) {
+            return;
+        }
+        if (xhr.status < 200 || xhr.status >= 300) {
+            alert("요청중 오류");
+            return;
+        }
+
+        const comments = JSON.parse(xhr.responseText);
+        updateComments(comments)
+    };
+    xhr.open('GET', `${origin}/api/article/comments?articleIndex=${articleIndex}&page=${page}`);
+    xhr.setRequestHeader(header, token);
+    xhr.send();
+}
+
+const updateComments = (comments) => {
+    console.log(comments);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadComments(1);
 });
