b553c52c4077bc932fe5b8c4bc145d541190feb2
prjsghq7 <prjsghq7@gmail.com>
2025-08-17 18:51:57 +0900

    [김건호, 게시글 댓글 등록] 게시글 댓글 등록 프론트 구현

---
+++ src/main/resources/static/article/assets/scripts/article.js
@@ -0,0 +1,28 @@
+const $commentForm = document.getElementById("commentForm");
+
+
+$commentForm.addEventListener('submit' , (e) => {
+   e.preventDefault();
+
+   //TODO regex 처리 추후
+
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+    xhr.append("articleIndex", new URL(location.href).searchParams.get('index'));
+    xhr.append("content", $commentForm['content'].value);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) {
+            alert("요청중 오류");
+            return;
+        }
+        if (xhr.status < 200 || xhr.status >= 300) {
+            alert("요청중 오류");
+            return;
+        }
+
+        const response = JSON.parse(xhr.responseText);
+        alert(response);
+    };
+    xhr.open('POST', 'api/article/uploads');
+    xhr.send(formData);
+});
