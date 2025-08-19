4f91fc0130f0a6f35f6386704fd4ee612301b92b
정현규 <hyeongyu-98@naver.com>
2025-08-19 18:18:59 +0900

    [정현규, notice] - 공지사항 notice 타임리프 수정.

--- src/main/resources/static/notice/assets/scripts/notice.js
+++ src/main/resources/static/notice/assets/scripts/notice.js
@@ -11,53 +11,57 @@ window.addEventListener('pageshow', (event) => {
     }
 });
 
-modifyBtn.addEventListener('click', () => {
-    const url = new URL(location.href);
-    const index = url.searchParams.get("index");
-    location.href = `${origin}/notice/modify?index=${index}`;
-})
+if ($modifyBtn !== null) {
+    modifyBtn.addEventListener('click', () => {
+        const url = new URL(location.href);
+        const index = url.searchParams.get("index");
+        location.href = `${origin}/notice/modify?index=${index}`;
+    })
+}
 
-deleteBtn.addEventListener('click', () => {
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData();
-    const url = new URL(location.href);
-    const index = url.searchParams.get("index");
-    formData.append("index", index);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState !== XMLHttpRequest.DONE) {
-            return;
-        }
-        if (xhr.status < 200 || xhr.status >= 300) {
-            alert('요청 중 오류');
-            return;
-        }
-        const response = JSON.parse(xhr.responseText);
-        const result = response.result;
-        switch (result) {
-            case 'success':
-                dialog.show({title: '삭제',
-                content:'삭제에 성공하셨습니다',
-                buttons:[
-                    {
-                        caption:'확인',
-                        color:'green',
-                        onClickCallback:($modal)=>{
-                            location.href = `${origin}/notice/`;
-                        }
-                    }
-                ]})
-                break;
-            case'failure':
-                dialog.showSimpleOk('삭제', '삭제에 실패하셨습니다.');
-                break;
-            case'failure_doesnt_exit':
-                dialog.showSimpleOk('삭제', '존재하지 않거나 이미 삭제된 공지사항입니다.');
-                break;
-            default:
-                break;
-        }
-    };
-    xhr.open('DELETE', '/api/notice/delete');
-    xhr.setRequestHeader(header, token)
-    xhr.send(formData);
-})
+if ($deleteBtn !== null) {
+    deleteBtn.addEventListener('click', () => {
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+        const url = new URL(location.href);
+        const index = url.searchParams.get("index");
+        formData.append("index", index);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) {
+                return;
+            }
+            if (xhr.status < 200 || xhr.status >= 300) {
+                alert('요청 중 오류');
+                return;
+            }
+            const response = JSON.parse(xhr.responseText);
+            const result = response.result;
+            switch (result) {
+                case 'success':
+                    dialog.show({title: '삭제',
+                        content:'삭제에 성공하셨습니다',
+                        buttons:[
+                            {
+                                caption:'확인',
+                                color:'green',
+                                onClickCallback:($modal)=>{
+                                    location.href = `${origin}/notice/`;
+                                }
+                            }
+                        ]})
+                    break;
+                case'failure':
+                    dialog.showSimpleOk('삭제', '삭제에 실패하셨습니다.');
+                    break;
+                case'failure_doesnt_exit':
+                    dialog.showSimpleOk('삭제', '존재하지 않거나 이미 삭제된 공지사항입니다.');
+                    break;
+                default:
+                    break;
+            }
+        };
+        xhr.open('DELETE', '/api/notice/delete');
+        xhr.setRequestHeader(header, token)
+        xhr.send(formData);
+    });
+}
